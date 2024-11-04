2a83214f08c4ddcfcf2bfc14a4247ec3033a5c3e
Rhythm Nain Kour <118470834+rhythmnain@users.noreply.github.com>
2024-11-04 20:15:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find All Triplets with Zero Sum/find-all-triplets-with-zero-sum.java
@@ -0,0 +1,77 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+// User function Template for Java
+class Solution {
+    public List<List<Integer>> findTriplets(int[] arr) {
+        // Your code here
+       List<List<Integer>> ans = new ArrayList<>();
+        int n = arr.length;
+        Map<Integer, List<int[]>> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int sum = arr[i] + arr[j];
+                if (!map.containsKey(sum))   map.put(sum, new ArrayList<>());
+                map.get(sum).add(new int[] {i, j});
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            int rem = -arr[i];
+            if (map.containsKey(rem)) {
+                for (int[] p : map.get(rem)) {
+                    if (p[0] < i && p[1] < i) {
+                        List<Integer> temp = Arrays.asList(p[0], p[1],i);
+                        ans.add(temp);
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+}
+
+//{ Driver Code Starts.
+
+class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while (t-- > 0) {
+            String line = read.readLine().trim();
+            String[] numsStr = line.split(" ");
+            int[] nums = new int[numsStr.length];
+            for (int i = 0; i < numsStr.length; i++) {
+                nums[i] = Integer.parseInt(numsStr[i]);
+            }
+
+            Solution obj = new Solution();
+            List<List<Integer>> res = obj.findTriplets(nums);
+            Collections.sort(res, (a, b) -> {
+                for (int i = 0; i < a.size(); i++) {
+                    if (!a.get(i).equals(b.get(i))) {
+                        return a.get(i) - b.get(i);
+                    }
+                }
+                return 0;
+            });
+            if (res.size() == 0) {
+                System.out.println("[]");
+            }
+            for (int i = 0; i < res.size(); i++) {
+                for (int j = 0; j < res.get(i).size(); j++) {
+                    System.out.print(res.get(i).get(j) + " ");
+                }
+                System.out.println();
+            }
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
