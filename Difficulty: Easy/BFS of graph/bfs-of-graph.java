d68822bdee33367aca9549c453c9e361344534e4
Rhythm Nain Kour <118470834+rhythmnain@users.noreply.github.com>
2025-04-03 15:29:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/BFS of graph/bfs-of-graph.java
+++ Difficulty: Easy/BFS of graph/bfs-of-graph.java
@@ -11,24 +11,27 @@ class Solution {
     // Function to return Breadth First Search Traversal of given graph.
     public ArrayList<Integer> bfs(ArrayList<ArrayList<Integer>> adj) {
         // code here
-        ArrayList<Integer> traversal = new ArrayList<>();
-        Queue<Integer> queue = new LinkedList<>();
-        int size = adj.size();
-        boolean[] visited = new boolean[size];
-        queue.add(0);
-        visited[0] = true;
-        while(!queue.isEmpty()){
-            int pop = queue.poll();
-            traversal.add(pop);
-            for(int i = 0 ; i < adj.get(pop).size() ; i++){
-                int current = adj.get(pop).get(i);
-                if(!traversal.contains(current) && visited[current] != true){
-                    queue.add(current);
-                    visited[current] = true;
+         int numVertex = adj.size();
+        boolean visited [] = new boolean[numVertex];
+        ArrayList<Integer> res = new ArrayList<>();
+        int startVertex = 0;
+        
+        // Logic for BFS
+        
+        ArrayDeque<Integer> q = new ArrayDeque<>();
+        q.add(startVertex);
+        visited[startVertex] = true;
+        while(q.size()>0){
+            int currNode = q.remove();
+            res.add(currNode);
+            for(int neighbour : adj.get(currNode)){
+                if(!visited[neighbour]){
+                    q.add(neighbour);
+                    visited[neighbour]=true;
                 }
             }
         }
-        return traversal;
+        return res;
     }
 }
 
