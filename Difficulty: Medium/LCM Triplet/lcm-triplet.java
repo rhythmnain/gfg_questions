207337beb5331eee9efe8b3d8c50e3432b889b8c
Rhythm Nain Kour <118470834+rhythmnain@users.noreply.github.com>
2025-07-18 21:26:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/LCM Triplet/lcm-triplet.java
@@ -0,0 +1,11 @@
+class Solution {
+    int lcmTriplets(int n) {
+        // code here
+          if(n<=2) return n;
+        if(n%2==0 && n%3==0)
+            return (n-2)*(n-1)*(n-3);
+        else if(n%2==0)
+            return n*(n-1)*(n-3);
+        return n*(n-1)*(n-2);
+    }
+}
