7dcc8cf8b46f47781c8797bd482a6f663c587f10
Rhythm Nain Kour <118470834+rhythmnain@users.noreply.github.com>
2025-12-22 21:38:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Row with max 1s/row-with-max-1s.java
@@ -0,0 +1,19 @@
+// User function Template for Java
+
+class Solution {
+    public int rowWithMax1s(int arr[][]) {
+        // code here
+        int n = arr.length;
+        int j = arr[0].length-1;
+        int ans = -1;
+        for(int i=0; i<n; i++){
+            while(j>=0 && arr[i][j]==1){
+               ans = i; 
+               j--;
+            }
+        
+        if(j==-1) return i;
+        }
+        return ans;
+    }
+}
